afc775cc1e90829f6392d8eea0de19fcbfce2f8c
Pablo Thobias <pablo.carminatti@newsela.com>
2023-11-27 17:46:42 -0300

    feature(Recipes): creating first template for recipes view

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def home_view(request):
-    return HttpResponse("<h1>Home Page</h1>")
+    return render(request, "home.html")
 
 
 def about_view(request):
