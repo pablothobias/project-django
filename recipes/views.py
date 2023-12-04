81eab2244b9cd183906889cb534889b570cdf679
Pablo Thobias <pablo.carminatti@newsela.com>
2023-12-04 14:24:22 -0300

    feature(Recipes): creating mock data to consume on templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 
 
 def recipes_home_page(request):
-    return render(request, "recipes/pages/home.html")
+    context = {"recipes": [make_recipe() for _ in range(10)]}
+    return render(request, "recipes/pages/home.html", context=context)
 
 
 def recipe_page(request, id):
-    context = {"id": id}
+    context = {"recipe": make_recipe()}
     return render(request, "recipes/pages/recipe-detail.html", context=context)
