b775c0c7d474a5eb8c2d965b96741894eaf23c79
Pablo Thobias <pablo.carminatti@newsela.com>
2023-12-06 16:54:54 -0300

    feature(Recipes): updating logic to return 404

--- recipes/views.py
+++ recipes/views.py
@@ -1,39 +1,34 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Recipe
 
-from utils.recipes.factory import make_recipe
-
-# Create your views here.
+# from utils.recipes.factory import make_recipe
 
 
 def recipes_home_page(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by("-id")
-
-    if not recipes:
-        raise Http404("Recipes not found 😢")
-
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            is_published=True,
+        ).order_by("-id")
+    )
     context = {"recipes": recipes}
+
     return render(request, "recipes/pages/home.html", context=context)
 
 
 def recipe_page(request, id):
-    recipe = Recipe.objects.get(id=id)
-
-    if not recipe:
-        raise Http404("Recipe not found 😢")
-
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
     context = {"is_detail_page": True, "recipe": recipe, "title": f"{recipe.title} | "}
+
     return render(request, "recipes/pages/recipe-detail.html", context=context)
 
 
 def category_page(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id, is_published=True
-    ).order_by("-id")
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by("-id")
+    )
+    context = {"recipes": recipes, "title": f"{recipes[0].category.name} | "}
 
-    if not recipes:
-        raise Http404("Category not found 😢")
-
-    context = {"recipes": recipes, "title": f"{recipes.first().category.name} | "}
     return render(request, "recipes/pages/category.html", context=context)
